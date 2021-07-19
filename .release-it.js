bbaf92405e4cd03bad009a8974ca207a2a78c6e8
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-07-19 14:18:31 +0800

    chore: update script

--- .release-it.js
+++ .release-it.js
@@ -13,7 +13,7 @@ module.exports = {
     release: false,
   },
   hooks: {
-    "before:init": "yarn run test",
+    "before:init": "yarn run lint",
     "after:git:release": "git push origin main:release -f"
   }
 };
