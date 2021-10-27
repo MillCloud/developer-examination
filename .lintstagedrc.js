8851b189bb1ecb4420e2790f8c1e706459740807
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-10-27 21:04:46 +0800

    chore: update config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-  '*.json': 'prettier --write',
   '*.{css,less,scss,vue}': 'stylelint --fix',
   '*.{js,jsx,ts,tsx,vue}': 'eslint --fix',
   '*.{md,markdown}': 'markdownlint --fix',
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
-import ReactRefresh from '@vitejs/plugin-react-refresh';
-import ReactJsx from 'vite-react-jsx';
-import StyleImport from 'vite-plugin-style-import';
+import React from '@vitejs/plugin-react';
 import TsconfigPaths from 'vite-tsconfig-paths';
 import WindiCSS from 'vite-plugin-windicss';
 import Pages from 'vite-plugin-pages';
@@ -11,26 +9,13 @@ import Pages from 'vite-plugin-pages';
 export default defineConfig({
   plugins: [
     Vue(),
-    ReactRefresh(),
-    ReactJsx(),
+    React(),
     TsconfigPaths(),
     WindiCSS(),
     Pages({
       react: true,
     }),
-    StyleImport({
-      libs: [
-        {
-          libraryName: 'antd',
-          esModule: true,
-          resolveStyle: (name) => `antd/es/${name}/style/index`,
-        },
-      ],
-    }),
   ],
-  build: {
-    chunkSizeWarningLimit: 1024,
-  },
   optimizeDeps: {
     include: [
       '@iconify/react',
