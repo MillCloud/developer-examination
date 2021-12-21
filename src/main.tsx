05a8b5b92985bd80363dd7a50d1f03e60e530e1b
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-12-21 10:49:38 +0800

    feat: windicss -> tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import { ConfigProvider } from 'antd';
 import zhCN from 'antd/lib/locale/zh_CN';
 import 'antd/dist/antd.variable.min.css';
 import { HashRouter, useRoutes } from 'react-router-dom';
+import '@/styles/global.css';
 import routes from '~react-pages';
-import Layout from '@/layout';
-import 'virtual:windi.css';
+import Layout from '@/layouts';
 
 function App() {
   return useRoutes(routes);
